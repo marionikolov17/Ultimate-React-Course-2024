09fa14712cca0a532ff446bc0db82529b40692a3
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-09-24 23:08:38 +0300

    Create Main component

--- react-quiz/src/App.jsx
+++ react-quiz/src/App.jsx
@@ -1,5 +1,17 @@
+import Header from "./Header";
+import Main from "./main";
+
 export default function App() {
     return (
-        <></>
+        <>
+            <div className="app">
+                <Header />
+
+                <Main>
+                    <p>1/15</p>
+                    <p>Questions</p>
+                </Main>
+            </div>
+        </>
     )
 }
--- react-quiz/src/main.jsx
+++ react-quiz/src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'
-import App from './App';
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+/* eslint-disable react/prop-types */
+export default function Main({ children }) {
+    return (
+        <>
+            <main className="main">
+                {children}
+            </main>
+        </>
+    )
+}
