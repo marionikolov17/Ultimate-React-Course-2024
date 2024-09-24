a788a2e3bc6773cda06f28ce6813411ace7e37e0
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-09-24 23:23:04 +0300

    Fetch questions with useReducer

--- react-quiz/src/App.jsx
+++ react-quiz/src/App.jsx
@@ -1,19 +1,56 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./main";
 
+const initialState = {
+  questions: [],
+
+  // "loading", "error", "ready", "active", "finished"
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Action is unknown");
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-    
-    return (
-        <>
-            <div className="app">
-                <Header />
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch({ type: "dataReceived", payload: data });
+      })
+      .catch((err) => {
+        dispatch({ type: "dataFailed" });
+      });
+  }, []);
 
-                <Main>
-                    <p>1/15</p>
-                    <p>Questions</p>
-                </Main>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="app">
+        <Header />
+
+        <Main>
+          <p>1/15</p>
+          <p>Questions</p>
+        </Main>
+      </div>
+    </>
+  );
 }
