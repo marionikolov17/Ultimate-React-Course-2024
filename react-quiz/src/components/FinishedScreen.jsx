999366f6b6a79edcba7a7082fa44cd8632165d61
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-09-25 21:43:36 +0300

    Create FinishedScreen.jsx

---
+++ react-quiz/src/components/FinishedScreen.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+export default function FinishedScreen({ points, maxPossiblePoints }) {
+    const percentage = (points / maxPossiblePoints) / 100;
+
+    return (
+        <>
+            <p>
+                You scored <strong>{points}</strong> out of {maxPossiblePoints} ({Math.ceil(percentage)})
+            </p>
+        </>
+    )
+}
