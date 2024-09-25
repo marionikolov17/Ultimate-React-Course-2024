d1e35e3cd44bf35dad4b34dfebe5af9414257d45
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-09-25 20:47:49 +0300

    Display questions

--- react-quiz/src/components/Question.jsx
+++ react-quiz/src/components/Question.jsx
@@ -1,10 +1,12 @@
+import Options from "./Options";
+
 /* eslint-disable react/prop-types */
 function Question({ question }) {
   return (
     <div>
       <h4>{question?.question}</h4>
 
-      
+      <Options question={question}/>
     </div>
   );
 }
