422ac0553c69ca20c26be9d481035d562c25ca80
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-09-25 21:33:05 +0300

    Create Progress.jsx

---
+++ react-quiz/src/components/Progress.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+export default function Progress({ index, numQuestions, points, maxPossiblePoints }) {
+    return (
+        <>
+            <header className="progress">
+                <p>Question <strong>{index + 1}</strong> / {numQuestions}</p>
+
+                <p><strong>{points}</strong> / {maxPossiblePoints}</p>
+            </header>
+        </>
+    )
+}
