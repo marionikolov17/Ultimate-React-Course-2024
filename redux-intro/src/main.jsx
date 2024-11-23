c673b5936a310c7a6b62bca488cd72412b1c6db0
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-11-23 19:13:38 +0200

    Dispatch actions from react application

--- redux-intro/src/main.jsx
+++ redux-intro/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from "react-redux"
 import './index.css'
 import App from './App.jsx'
 
@@ -7,6 +8,8 @@ import store from './store.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
