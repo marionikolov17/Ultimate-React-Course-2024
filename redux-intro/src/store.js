4aa2458c0638f8362be3c5a822463a1b2876a13d
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-11-23 18:32:54 +0200

    Create customer state and root reducer

--- redux-intro/src/store.js
+++ redux-intro/src/store.js
@@ -1,29 +1,69 @@
-import { createStore } from "redux";
+import { combineReducers, createStore } from "redux";
 
-const initialState = {
-    balance: 0,
-    loan: 0,
-    loanPurpose: ""
+const initialStateAccount = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+
+const initialStateCustomer = {
+  fullName: "",
+  nationalID: "",
+  createdAt: "",
+};
+
+function accountReducer(state = initialStateAccount, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return { ...state, balance: state.balance + action.payload };
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+
+    default:
+      return state;
+  }
 }
 
-function reducer(state = initialState, action) {
-    switch(action.type) {
-        case "account/deposit":
-            return {...state, balance: state.balance + action.payload};
-        case "account/withdraw":
-            return {...state, balance: state.balance - action.payload};
-        case "account/requestLoan":
-            if (state.loan > 0) return state;
-            return {...state, loan: action.payload.amount, loanPurpose: action.payload.purpose, balance: state.balance + action.payload.amount};
-        case "account/payLoan":
-            return {...state, loan: 0, loanPurpose: "", balance: state.balance - state.loan}
-
-        default:
-            return state;
-    }
+function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalId: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      };
+    case "customer/updateName":
+        return {
+            ...state,
+            fullName: action.payload
+        }
+    default:
+      return state;
+  }
 }
 
-const store = createStore(reducer);
+const rootReducer = combineReducers({
+    account: accountReducer,
+    customer: customerReducer
+})
+
+const store = createStore(rootReducer);
 
 /* store.dispatch({ type: "account/deposit", payload: 500 });
 store.dispatch({ type: "account/withdraw", payload: 200 });
@@ -32,19 +72,19 @@ store.dispatch({ type: "account/requestLoan", payload: { amount: 1000, purpose: 
 store.dispatch({ type: "account/payLoan" }); */
 
 function deposit(amount) {
-    return { type: "account/deposit", payload: amount };
+  return { type: "account/deposit", payload: amount };
 }
 
 function withdraw(amount) {
-    return { type: "account/withdraw", payload: amount }
+  return { type: "account/withdraw", payload: amount };
 }
 
 function requestLoan(amount, purpose) {
-    return { type: "account/requestLoan", payload: { amount, purpose } }
+  return { type: "account/requestLoan", payload: { amount, purpose } };
 }
 
 function payLoan() {
-    return { type: "account/payLoan" };
+  return { type: "account/payLoan" };
 }
 
 store.dispatch(deposit(500));
@@ -55,3 +95,22 @@ store.dispatch(requestLoan(1000, "Buy a car"));
 console.log(store.getState());
 store.dispatch(payLoan());
 console.log(store.getState());
+
+function createCustomer(fullName, nationalID) {
+  return {
+    type: "customer/createCustomer",
+    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
+  };
+}
+
+function updateName(name) {
+  return {
+    type: "customer/updateName",
+    payload: name,
+  };
+}
+
+store.dispatch(createCustomer("Mario Nikolov", "0943829"));
+store.dispatch(deposit(250));
+console.log(store.getState());
+store.dispatch(updateName("Nikolov Mario"));
