a4e192dbc7f67dd1c62caf19ede83b619144da2b
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-11-26 22:05:05 +0200

    Create new browser router from react router v6.4+

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -1,9 +1,19 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/menu", element: <Menu /> },
+  { path: "/order/new", element: <CreateOrder /> },
+  { path: "/order/:orderId", element: <Order /> },
+])
+
 function App() {
-  return (
-    <>
-      <div>Hello Vite!</div>
-    </>
-  );
+  return <RouterProvider router={router}/>
 }
 
 export default App;
