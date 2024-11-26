b8d83376eade21b6c1514c47642f9b054cfe9fc1
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-11-26 23:13:53 +0200

    Fetch orders

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./ui/Home";
 import Error from "./ui/Error";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 import Cart from "./features/cart/Cart";
 
@@ -22,7 +22,12 @@ const router = createBrowserRouter([
       },
       { path: "/cart", element: <Cart /> },
       { path: "/order/new", element: <CreateOrder /> },
-      { path: "/order/:orderId", element: <Order /> },
+      { 
+        path: "/order/:orderId", 
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />
+      },
     ],
   },
 ]);
