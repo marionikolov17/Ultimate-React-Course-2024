e8cfc3a9dc476e9799432716a4de54e036b1bcc2
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-11-27 17:40:32 +0200

    Create order using react router actions

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -3,7 +3,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./ui/Home";
 import Error from "./ui/Error";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
-import CreateOrder from "./features/order/CreateOrder";
+import CreateOrder, { action as createOrderAction } from "./features/order/CreateOrder";
 import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 import Cart from "./features/cart/Cart";
@@ -21,7 +21,11 @@ const router = createBrowserRouter([
         errorElement: <Error />
       },
       { path: "/cart", element: <Cart /> },
-      { path: "/order/new", element: <CreateOrder /> },
+      { 
+        path: "/order/new", 
+        element: <CreateOrder />,
+        action: createOrderAction
+      },
       { 
         path: "/order/:orderId", 
         element: <Order />,
